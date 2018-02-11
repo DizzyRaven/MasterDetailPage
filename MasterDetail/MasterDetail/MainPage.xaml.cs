b9f553ea36a7d5a90e55b71ffe06d4cfcfc6ee0a
DizzyRaven <akaynt47@gmail.com>
2018-02-11 20:10:57 +0200

    Done

--- MasterDetail/MasterDetail/MainPage.xaml.cs
+++ MasterDetail/MasterDetail/MainPage.xaml.cs
@@ -7,11 +7,44 @@ using Xamarin.Forms;
 
 namespace MasterDetail
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage : MasterDetailPage
 	{
-		public MainPage()
-		{
-			InitializeComponent();
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Свойство detailPage, отвечает за ditail страницу(главную, кторая сзади)
+            Detail = new NavigationPage(new Page1())
+            {
+                BarBackgroundColor = Color.DarkOliveGreen//Head color
+            };
+
+            //Is presented прячет Master page
+            IsPresented = false;
+
 		}
-	}
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page1());
+            IsPresented = false;
+        }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page2());
+            IsPresented = false;
+        }
+        public void swch1(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+            {
+                Detail.BackgroundColor = Color.Yellow;
+                nm.Text = "On";
+            }
+            else
+            {
+                Detail.BackgroundColor = Color.White;
+                nm.Text = "Off";
+            }
+        }
+    }
 }
